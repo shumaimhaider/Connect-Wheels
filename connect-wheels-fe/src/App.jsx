b132c51aaad2274b2692dacf27efd55d3ed4c222
shumaimhaider <shumaimhaider@gmail.com>
2025-09-04 23:26:19 +0500

    Fix config

--- connect-wheels-fe/src/App.jsx
+++ connect-wheels-fe/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import {React, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-     
+      <img src={viteLogo} alt="Vite logo" />
+      <img src={reactLogo} alt="React logo" />
+      <h1>Hello Vite + React!</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Increase</button>
     </>
   )
 }
