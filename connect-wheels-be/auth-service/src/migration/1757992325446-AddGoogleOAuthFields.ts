bc4e6df55cedc6a7b549e1b8ee1c16fe93f4e6e8
Salman Ali <salman.hanif524@gmial.com>
2025-09-17 07:12:48 +0500

    handle google login

--- connect-wheels-be/auth-service/src/migration/1757992325446-AddGoogleOAuthFields.ts
+++ connect-wheels-be/auth-service/src/migration/1757992325446-AddGoogleOAuthFields.ts
@@ -1,16 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddGoogleFields1757445406617 implements MigrationInterface {
-    name = 'AddGoogleFields1757445406617'
-
+export class AddGoogleOAuthFields1757992325446 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // Only add the two Google-specific fields
         await queryRunner.query(`ALTER TABLE "app_user" ADD "googleId" character varying`);
-        await queryRunner.query(`ALTER TABLE "app_user" ADD "email" character varying`);
+        await queryRunner.query(`ALTER TABLE "app_user" ADD "googleRefreshToken" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "app_user" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "app_user" DROP COLUMN "googleRefreshToken"`);
         await queryRunner.query(`ALTER TABLE "app_user" DROP COLUMN "googleId"`);
     }
-
 }
